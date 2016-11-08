79353816c9d569ef5b36bb5f335865afca2486bf
Thiago Bertuzzi <thiago.bertuzzi@gmail.com>
2016-11-08 15:25:34 -0200

    Versão Inicial

---
+++ MLSearch/frmMain.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace MLSearch
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtPesquisa = new System.Windows.Forms.TextBox();
+            this.lvProdutos = new System.Windows.Forms.ListView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pbProduto = new System.Windows.Forms.PictureBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblVendedor = new System.Windows.Forms.Label();
+            this.lblPreco = new System.Windows.Forms.Label();
+            this.lblurl = new System.Windows.Forms.LinkLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.pbProduto)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtPesquisa
+            // 
+            this.txtPesquisa.Location = new System.Drawing.Point(12, 12);
+            this.txtPesquisa.Name = "txtPesquisa";
+            this.txtPesquisa.Size = new System.Drawing.Size(710, 20);
+            this.txtPesquisa.TabIndex = 1;
+            this.txtPesquisa.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtPesquisa_KeyDown);
+            // 
+            // lvProdutos
+            // 
+            this.lvProdutos.Location = new System.Drawing.Point(12, 74);
+            this.lvProdutos.Name = "lvProdutos";
+            this.lvProdutos.Size = new System.Drawing.Size(556, 159);
+            this.lvProdutos.TabIndex = 2;
+            this.lvProdutos.UseCompatibleStateImageBehavior = false;
+            this.lvProdutos.SelectedIndexChanged += new System.EventHandler(this.lvProdutos_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Resultado";
+            // 
+            // pbProduto
+            // 
+            this.pbProduto.Location = new System.Drawing.Point(574, 74);
+            this.pbProduto.Name = "pbProduto";
+            this.pbProduto.Size = new System.Drawing.Size(148, 159);
+            this.pbProduto.TabIndex = 4;
+            this.pbProduto.TabStop = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblurl);
+            this.groupBox1.Controls.Add(this.lblPreco);
+            this.groupBox1.Controls.Add(this.lblVendedor);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(15, 239);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(713, 100);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Dados";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Vendedor:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(17, 52);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Preço:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(17, 75);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(32, 13);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "URL:";
+            // 
+            // lblVendedor
+            // 
+            this.lblVendedor.AutoSize = true;
+            this.lblVendedor.Location = new System.Drawing.Point(79, 30);
+            this.lblVendedor.Name = "lblVendedor";
+            this.lblVendedor.Size = new System.Drawing.Size(53, 13);
+            this.lblVendedor.TabIndex = 3;
+            this.lblVendedor.Text = "Vendedor";
+            // 
+            // lblPreco
+            // 
+            this.lblPreco.AutoSize = true;
+            this.lblPreco.Location = new System.Drawing.Point(79, 52);
+            this.lblPreco.Name = "lblPreco";
+            this.lblPreco.Size = new System.Drawing.Size(35, 13);
+            this.lblPreco.TabIndex = 4;
+            this.lblPreco.Text = "Preço";
+            // 
+            // lblurl
+            // 
+            this.lblurl.AutoSize = true;
+            this.lblurl.Location = new System.Drawing.Point(79, 75);
+            this.lblurl.Name = "lblurl";
+            this.lblurl.Size = new System.Drawing.Size(20, 13);
+            this.lblurl.TabIndex = 5;
+            this.lblurl.TabStop = true;
+            this.lblurl.Text = "Url";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(740, 343);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.pbProduto);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lvProdutos);
+            this.Controls.Add(this.txtPesquisa);
+            this.MaximizeBox = false;
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MLSearch";
+            ((System.ComponentModel.ISupportInitialize)(this.pbProduto)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtPesquisa;
+        private System.Windows.Forms.ListView lvProdutos;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pbProduto;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.LinkLabel lblurl;
+        private System.Windows.Forms.Label lblPreco;
+        private System.Windows.Forms.Label lblVendedor;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ MLSearch/frmMain.cs
@@ -0,0 +1,72 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+using System.Windows.Forms;
+
+namespace MLSearch
+{
+    public partial class frmMain : Form
+    {
+        dynamic retorno;
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void txtPesquisa_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                string apiUrl = "https://api.mercadolibre.com";
+                RestClient client = new RestClient(apiUrl);
+
+                var request = new RestRequest("/sites/MLB/search?q=" + txtPesquisa.Text + "&limit=5000");
+                request.AddHeader("Accept", "application/json");
+                request.AddHeader("Content-Type", "application/json");
+                request.RequestFormat = DataFormat.Json;
+
+                var response = client.Execute(request);
+
+                var serializer = new JavaScriptSerializer();
+                serializer.RegisterConverters(new[] { new DynamicJsonConverter() });
+
+                retorno = serializer.Deserialize(response.Content, typeof(object));
+
+                lvProdutos.Items.Clear();
+                for (int i = 0; i < retorno.results.Count; i++)
+                {
+                    ListViewItem lvi = new ListViewItem();
+                    lvi.Text = retorno.results[i].title;
+                    lvProdutos.Items.Add(lvi);
+                }
+               
+
+            }
+        }
+
+        private void lvProdutos_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ListView lvi = (ListView)sender;
+            WebClient wc = new WebClient();
+            byte[] bytes = wc.DownloadData(retorno.results[lvi.FocusedItem.Index].thumbnail);
+            MemoryStream ms = new MemoryStream(bytes);
+            System.Drawing.Image img = System.Drawing.Image.FromStream(ms);
+
+            pbProduto.Image = img;
+
+            lblPreco.Text = Convert.ToString(retorno.results[lvi.FocusedItem.Index].price);
+            lblVendedor.Text = retorno.results[lvi.FocusedItem.Index].seller.power_seller_status;
+            lblurl.Text = retorno.results[lvi.FocusedItem.Index].permalink;
+
+        }
+    }
+}
